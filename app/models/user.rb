d98fd5d8e0e98afbf38544caf874a9c677eff9a7
Mick Johnson <mickajohnson@email.com>
2017-03-29 01:29:22 +0000

    Added state to algo

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
    validates :username, uniqueness: true
    validates :username, :birthday, presence: true
    validates :password, confirmation: true, length: {minimum: 7}, on: :create
+   validates :zipcode, length: {is:5}
    has_many :user_interests
    has_many :interests, class_name: "Interest", through: 'user_interests'
    has_many :pictures
@@ -16,4 +17,14 @@ class User < ApplicationRecord
    has_many :matches
    has_many :messages
    has_many :favorites
+
+   before_save :add_state_city
+
+   private
+   def add_state_city
+     location = ZipCodes.identify(self.zipcode.to_s)
+     self.city = location[:city]
+     self.state = location[:state_code]
+   end
+
 end
